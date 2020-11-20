553330536addd89bcf8b18723dac813f83bbe12d
Hyeonju Kim <hyeonjukm01@gmail.com>
2020-11-19 18:50:47 -0600

    delete codecov in travis

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ makedocs(;
     modules=[flxQTL],
     authors="Hyeonju Kim <hyeonjukm01@gmail.com>",
     repo="https://github.com/hkim89/flxQTL.jl/blob/{commit}{path}#L{line}",
-    sitename="flxQTL.jl",
+    sitename="flxQTL Documentation",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://hkim89.github.io/flxQTL.jl/stable",
