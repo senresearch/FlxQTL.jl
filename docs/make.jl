3e10b2152685cfe80aa9dc7e6730883fbc391ad3
Hyeonju Kim <hyeonjukm01@gmail.com>
2020-11-19 19:14:26 -0600

    test makefile

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,12 @@ makedocs(;
     modules=[flxQTL],
     authors="Hyeonju Kim <hyeonjukm01@gmail.com>",
 #     repo="https://github.com/hkim89/flxQTL.jl/blob/{commit}{path}#L{line}",
-    sitename="flxQTL Documentation",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://hkim89.github.io/flxQTL.jl/stable",
-        assets=String[],
-    ),
+    sitename="flxQTL.jl",
+#     format=Documenter.HTML(;
+#         prettyurls=get(ENV, "CI", "false") == "true",
+#         canonical="https://hkim89.github.io/flxQTL.jl/stable",
+#         assets=String[],
+#     ),
 #     doctest=false,
     pages=[
         "Home" => "index.md",
