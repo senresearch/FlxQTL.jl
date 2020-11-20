51f2ec135cf48d0357a98af67e3a055c0ce50555
Hyeonju Kim <hyeonjukm01@gmail.com>
2020-11-19 19:20:01 -0600

    test 2

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ makedocs(;
     authors="Hyeonju Kim <hyeonjukm01@gmail.com>",
 #     repo="https://github.com/hkim89/flxQTL.jl/blob/{commit}{path}#L{line}",
     sitename="flxQTL.jl",
-#     format=Documenter.HTML(;
-#         prettyurls=get(ENV, "CI", "false") == "true",
-#         canonical="https://hkim89.github.io/flxQTL.jl/stable",
-#         assets=String[],
-#     ),
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://hkim89.github.io/flxQTL.jl/",
+        assets=String[],
+    ),
 #     doctest=false,
     pages=[
         "Home" => "index.md",
